a5b5ee74f6f00a87d8736465764fc6f18ec7a559
shivamuniyal36497 <shivamuniyal768@gmail.com>
2026-08-13 22:13:16 +0530

    Update Q001.c

--- Q001.c
+++ Q001.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
-int main() {
-int a = 7 , b = 5 , c = a+b;
-printf("%d", c);
-    return 0;
+int main()
+{
+   float a,b,c;
 
+   printf("enter value of a: ");
+   scanf("%f",&a);
+
+    printf("enter value of b: ");
+    scanf("%f",&b);
+
+    c = a+b;
+
+      printf("sum = %f",c);
+
+    return 0;
 }
